d9b2960b8c0dadba89687762a9ad6753154c4b23
Ohad Keshet <53540070+ohadke@users.noreply.github.com>
2021-05-21 13:47:25 +0300

    first commit

---
+++ TodoApi/Configurations/ToDoConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Configurations
+{
+    public class ToDoConfiguration : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.ToTable("Todos");//Name of the table
+            builder.HasKey(t => t.Id);//Choose the Primary Key
+            builder.Property(t => t.IsComplete).IsRequired();//Add column to the table, cannot be empty when inserted
+            builder.Property(t => t.Name).IsRequired();//Add column to the table, cannot be empty when inserted
+            builder.Property(t => t.Secret);//Add column to the table, CAN be empty when inserted
+        }
+    }
+}
---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Configurations;
+
+namespace TodoApi.Models
+{
+    //This Class is responsible for the communication between the process and the DataBase
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoItem).Assembly); //Option 1 of how to configure a Class to a Table
+            //modelBuilder.ApplyConfiguration<TodoItem>(new ToDoConfiguration()); //Option 2
+        }
+    }
+}
